1e225cbb101ee4678642fa7da28051c9c40ef5d6
Samuel <samuel@pop-os.localdomain>
2022-11-05 00:13:58 -0300

    adjust route map

--- src/pages/deliveries/deliveries.styles.js
+++ src/pages/deliveries/deliveries.styles.js
@@ -10,4 +10,5 @@ export const Container = styled.div`
   padding: 20px;
   display: flex;
   flex-direction: column;
+  overflow: auto;
 `;
--- src/pages/routeMap/routeMap.screen.js
+++ src/pages/routeMap/routeMap.screen.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Layout from "../../components/layout";
+import { Title } from "../../styles/globalStyles";
+import { Container, Direction, GoBack } from "./routeMap.syles";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const RouteMapScreen = () => {
   return (
     <Layout
       title="Mapa"
       contentStyle={{ justifyContent: "center", alignItems: "center" }}
-    ></Layout>
+    >
+      <Direction>
+        <Link to="/lista-de-entregas" style={{ textDecoration: "none" }}>
+          <GoBack>
+            <FaArrowAltCircleLeft className="go-back" />
+            <Title>Voltar</Title>
+          </GoBack>
+        </Link>
+
+        <Container></Container>
+      </Direction>
+    </Layout>
   );
 };
 
